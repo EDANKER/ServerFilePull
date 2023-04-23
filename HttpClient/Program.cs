32a4385fddf1956e032def24aebb4c97ec587b20
edankryzo <eledyall@bk.ru>
2023-04-23 23:25:47 +0500

    ServerWhile

--- HttpClient/Program.cs
+++ HttpClient/Program.cs
@@ -9,19 +9,23 @@ namespace Microsoft
     {
         private static async Task Main(string[] args)
         {
-            HttpListener server = new HttpListener();
-            
-            server.Prefixes.Add("http://10.3.8.227:8888/connection/");
-            server.Start();
+            while (true)
+            {
 
-            var context = await server.GetContextAsync();
-            
-            var request = context.Request;
-            var response = context.Response;
-            var user = context.User;
-            
-            string responseText = 
-                @"<!DOCTYPE html>
+
+                HttpListener server = new HttpListener();
+
+                server.Prefixes.Add("http://10.3.8.227:8888/connection/");
+                server.Start();
+
+                var context = await server.GetContextAsync();
+
+                var request = context.Request;
+                var response = context.Response;
+                var user = context.User;
+
+                string responseText =
+                    @"<!DOCTYPE html>
                 <html>
                     <head>
                        <meta charset='utf8'>
@@ -32,18 +36,19 @@ namespace Microsoft
                     </body>
                 </html>";
 
-            byte[] bufer = Encoding.UTF8.GetBytes(responseText);
+                byte[] bufer = Encoding.UTF8.GetBytes(responseText);
 
-            response.ContentLength64 = bufer.Length;
+                response.ContentLength64 = bufer.Length;
 
-            using Stream output = response.OutputStream;
+                using Stream output = response.OutputStream;
 
-            await output.WriteAsync(bufer);
-            await output.FlushAsync();
+                await output.WriteAsync(bufer);
+                await output.FlushAsync();
 
-            Console.WriteLine("запрос обработан");
-            
-            
+                Console.WriteLine("запрос обработан");
+            }
+
+
         }
     }
     
